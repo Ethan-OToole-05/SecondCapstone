2a38a9c6a0bef75a7b4d4206dcb8086a63857080
Ethan O'Toole <eotoole005@gmail.com>
2021-11-04 13:34:12 -0400

    Account_Services_Done

---
+++ TenmoClient/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace TenmoClient.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public int UserId { get; set; }
+        public decimal Balance { get; set; }
+
+        public Account()
+        {
+
+        }
+        public Account(int acccountId, int userId, decimal balance)
+        {
+            AccountId = acccountId;
+            UserId = userId;
+            Balance = balance;
+        }
+
+    }
+}
---
+++ TenmoServer/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TenmoServer.DAO;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    public class UserController
+    {
+        //Get single user
+        private readonly IUserDAO userDAO;
+
+        public UserController(IUserDAO _userDAO)
+        {
+            userDAO = _userDAO;
+        }
+        public List<User> GetAllUsers()
+        {
+            List<User> Users = userDAO.GetUsers();
+            if(Users.Count > 0)
+            {
+                return Users;
+            }
+            else
+            {
+                throw new HttpRequestException("Error Occured.");
+            }
+        }
+    }
+}
