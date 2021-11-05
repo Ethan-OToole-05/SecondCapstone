5ae712bf9a5db589ad68b917aec18a8cc92b24b7
Jake Froehlich <jake.j.froehlich@gmail.com>
2021-11-04 20:16:43 -0400

    re-added iTransferDAO

---
+++ TenmoServer/DAO/ITransferDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    interface ITransferDAO
+    {
+        List<Transfer> ListByUser(int userId, bool pending);
+        Transfer GetTransferById(int id);
+        Transfer Create(Transfer transfer);
+    }
+}
