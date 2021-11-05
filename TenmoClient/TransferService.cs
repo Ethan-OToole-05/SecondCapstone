d23742e047521b189e5f7d83cfeb04446fec63d6
Jake Froehlich <jake.j.froehlich@gmail.com>
2021-11-04 21:28:17 -0400

    forgot to save transferservice

--- TenmoClient/TransferService.cs
+++ TenmoClient/TransferService.cs
@@ -11,8 +11,17 @@ namespace TenmoClient
         private readonly static string API_BASE_URL = "https://localhost:44315/";
         private readonly IRestClient client = new RestClient();
 
-        public Transfer SendTransfer(int sendingUserId, int receivingUserId, decimal amount)
+        public bool SendTransfer(int sendingUserId, int receivingUserId, decimal amount)
         {
+
+            RestRequest request = new RestRequest(API_BASE_URL + "api/account/user/" + $"{sendingUserId}");
+            IRestResponse<Account> accountResponse = client.Get<Account>(request);
+            if (accountResponse.Data.Balance < amount)
+            {
+                Console.WriteLine("Cannot send less money than is currently available.");
+                return false;
+            }
+
             Transfer newTransfer = new Transfer()
             {
                 TransferStatusId = 2,
@@ -22,33 +31,119 @@ namespace TenmoClient
                 Amount = amount
             };
 
-            IRestRequest request = new RestRequest(API_BASE_URL + "")
+            request = new RestRequest(API_BASE_URL + "api/transfers");
+            request.AddJsonBody(newTransfer);
+            IRestResponse<Transfer> response = client.Post<Transfer>(request);
 
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("An error occurred communicating with the server.");
+                return false;
+            }
+            else if (!response.IsSuccessful)
+            {
+
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                return false;
+            }
+            else
+            {
+                accountResponse.Data.Balance -= amount;
+                request = new RestRequest(API_BASE_URL + "api/account/balance/" + $"{sendingUserId}");
+                request.AddJsonBody(accountResponse.Data);
+                IRestResponse sendingAccountResponse = client.Put<Account>(request);
+                if (sendingAccountResponse.ResponseStatus != ResponseStatus.Completed)
+                {
+                    Console.WriteLine("An error occurred communicating with the server.");
+                    return false;
+                }
+                else if (!sendingAccountResponse.IsSuccessful)
+                {
+
+                    Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                    return false;
+                }
+                else
+                {
+                    request = new RestRequest(API_BASE_URL + "api/account/user/" + $"{receivingUserId}");
+                    IRestResponse<Account> receivingAccountResponse = client.Get<Account>(request);
+                    //Error handle response above
+                    receivingAccountResponse.Data.Balance += amount;
+                    request = new RestRequest(API_BASE_URL + "api/account/balance/" + $"{receivingUserId}");
+                    request.AddJsonBody(receivingAccountResponse.Data);
+                    IRestResponse accountReceivedResponse = client.Put<Account>(request);
+                    if (accountReceivedResponse.ResponseStatus != ResponseStatus.Completed)
+                    {
+                        Console.WriteLine("An error occurred communicating with the server.");
+                        return false;
+                    }
+                    else if (!accountReceivedResponse.IsSuccessful)
+                    {
+
+                        Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
         }
 
         public Transfer RequestTransfer(Transfer transfer)
         {
-
+            //TODO;
         }
 
-        public List<Transfer> GetUserTransfers(int userId)
+        public List<Transfer> GetUsersTransfers(int userId)
         {
-
+            RestRequest request = new RestRequest(API_BASE_URL + "api/transfers/user/" + $"{userId}");
+            IRestResponse<List<Transfer>> response = client.Get<List<Transfer>>(request);
+            if (response.Data.Count > 0)
+            {
+                Console.WriteLine("Could not find any transfers for the given Id.");
+                return null;
+            }
+            else
+            {
+                return response.Data;
+            }
         }
 
         public List<Transfer> GetPendingTransfers(int userId)
         {
-
+            RestRequest request = new RestRequest(API_BASE_URL + "api/transfers/user/" + $"{userId}/" + "pending");
+            IRestResponse<List<Transfer>> response = client.Get<List<Transfer>>(request);
+            if (response.Data.Count > 0)
+            {
+                Console.WriteLine("Could not find any pending transfers for the given Id.");
+                return null;
+            }
+            else
+            {
+                return response.Data;
+            }
         }
 
         public Transfer GetTransferDetails(int transferId)
         {
-
+            RestRequest request = new RestRequest(API_BASE_URL + "api/transfers/" + $"{transferId}");
+            IRestResponse<Transfer> response = client.Get<Transfer>(request);
+            if (response.Data == null)
+            {
+                Console.WriteLine("Could not find any transfers for the given Id.");
+                return null;
+            }
+            else
+            {
+                return response.Data;
+            }
         }
 
         public Transfer UpdateTransferStatus(int transferId, int updatedStatusId)
         {
-
+            //TODO;
         }
     }
 }
